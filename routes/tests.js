cf7b88d260b09ddae0de19e390e125cc97504977
deanly88 <lee_namyoung@hotmail.com>
2016-01-22 14:46:05 +0000

    20160122_2346

--- routes/tests.js
+++ routes/tests.js
@@ -14,8 +14,16 @@ router.get('/practice', function(req, res, next) {
   res.render('test/practice', { title: 'Promies' });
 });
 
+router.get('/practice2', function(req, res, next) {
+  res.render('test/practice2', { title: 'Promies' });
+});
+
 router.get('/canvas', function(req, res, next) {
   res.render('test/canvas', { title: 'Promies' });
+});
+
+router.get('/backbonejs', function(req, res, next) {
+  res.render('test/backbonejs', { title: 'Promies' });
 });
 
 module.exports = router;
